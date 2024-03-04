0e6326b557766c6f1996a0f75453428c23bf8341
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-04 21:46:57 +0200

    files updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,14 @@
 import { PageTitle } from "./PageTitle/PageTitle";
+import { EventBoard } from "./EventBoard/EventBoard";
+import upcomingEvents from '../upcoming-events.json';
 
 export const App = () => {
-  return <div><PageTitle/></div>;
+  return (
+    <div>
+      <PageTitle text='25th Core Worlds Coalition Conference' />
+      <EventBoard events={upcomingEvents } />
+    </div>
+
+  )
 }
 
--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,3 +1,10 @@
-export const EventBoard = () => {
-    return <div>Event cards</div>
+import css from './EventBoard.module.css'
+
+export const EventBoard = ({ events }) => {
+    // console.log(events)
+    return (
+        <div className={css.eventBoard}>
+            {events.map(event => <div key = {event.name}>{event.name }</div>)}
+        </div>
+    )
 }
